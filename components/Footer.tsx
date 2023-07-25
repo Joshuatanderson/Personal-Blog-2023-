da8d6d0974e3f5f4d9de0879c672821ab3acfb06
Joshua Anderson <joshuatanderson1@gmail.com>
2023-07-25 13:16:03 -0500

    feat: add icons w react-feather

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { globals } from '../globals';
+import React from 'react'
+import { globals } from '../globals'
+import { Rss } from 'react-feather'
 
 export const Footer: React.FC = () => (
-  <div className="footer">
-    <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
-    <a href="/rss.xml">
-      <img src="/img/rss-white.svg" alt="RSS Feed" height="30" width="30" />
-    </a>
-  </div>
-);
+    <div className="footer">
+        <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
+        <a href="/rss.xml">
+            <Rss />
+        </a>
+    </div>
+)
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { globals } from '../globals'
+import { GitHub, Linkedin } from 'react-feather'
 
 export const Header: React.FC = () => (
     <div className="header">
         <a href="/">{globals.siteName}</a>
         <div className="flex-spacer" />
-        <a href="https://github.com/Joshuatanderson?tab=repositories">GitHub</a>
-        <a href="https://www.linkedin.com/in/joshua-anderson-mba-2aab9b89/">
-            Linkedin
+        <a
+            href="https://github.com/Joshuatanderson?tab=repositories"
+            alt-text="github"
+        >
+            <GitHub />
+        </a>
+        <a
+            href="https://www.linkedin.com/in/joshua-anderson-mba-2aab9b89/"
+            alt-text="linkedin"
+        >
+            <Linkedin />
         </a>
     </div>
 )
