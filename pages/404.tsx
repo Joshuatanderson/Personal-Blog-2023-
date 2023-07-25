41982f2cea76f7a8342bfe2488b4833d27362379
Joshua Anderson <joshuatanderson1@gmail.com>
2023-07-25 11:17:26 -0500

    feat: add 404

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Custom404 = () => {
+    return (
+        <div>
+            <div className="blog-post-title">
+                <h1>404 - Page not found.</h1>
+                <p>Uh-oh. You have delved too deep. YOU SHALL NOT PASS!</p>
+            </div>
+
+            <img src="/img/gandalf-404.png" className="blog-post-image"></img>
+        </div>
+    )
+}
+
+export default Custom404
